e8c9621957ca9e955dbecccb6f5911f6d40a7e7f
Nic Ciccone <nicciccone1205@gmail.com>
2025-07-21 18:58:35 -0400

    first commit

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
-import db from "#db/client";
+import db from "./db/client";
+
+import { createFile } from "./db/queries/files";
+import { createFolder } from "./db/queries/folders";
 
 await db.connect();
 await seed();
@@ -6,5 +9,10 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 3; i++) {
+    const folder = await createFolder("Folder " + i);
+    for (let j = 1; j <= 5; j++) {
+      await createFile("File " + j, 1000 * j, folder.id);
+    }
+  }
 }
